7dd6c12ae1459d2d600ddb14f6068f3e75f099fe
J.Semikov <jsemikov@gmail.com>
2015-02-03 16:26:28 +0200

    one more
    commit

--- av.sh
+++ av.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 find /usr/local/ispmgr/var/userconf/ -print | while read LINE
 do
-USERNAME=$(echo $LINE | awk -F'.' '{print $2}')
-USERMAIL=$(cat $LINE | grep Email | awk {'print $2'})
+ USERNAME=$(echo $LINE | awk -F'.' '{print $2}')
+ USERMAIL=$(cat $LINE | grep Email | awk {'print $2'})
 if ! [ -d /var/www/$USERNAME/data/quarantine/ ]; then
-mkdir /var/www/$USERNAME/data/quarantine/
+ mkdir /var/www/$USERNAME/data/quarantine/
 fi
 	maldet -co quardir=/var/www/$USERNAME/data/quarantine/,email_addr=$USERMAIL  -a /var/www/$USERNAME/data/www/vir2/ &> /tmp/scan-$USERNAME
 chown -R $USERNAME:$USERNAME /var/www/$USERNAME/data/quarantine/
